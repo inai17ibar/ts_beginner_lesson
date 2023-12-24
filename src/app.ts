995a69b741abfecdef93b970c67e4d8ef0829b0a
Soichiro Inatani <inai17ibar@gmail.com>
2023-12-24 12:11:31 +0900

    solved Problem~6

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-console.log(getHelloWithDate())
+console.log(mergeString('俺は', '明日', '勉強する')) // 俺は明日勉強する
 
 /**
  *  「こんにちは」を取得する
@@ -27,4 +27,42 @@ function getHelloWithDate(): string
     }else {
         return "おやすみ"
     }
+}
+
+/**
+ *  0から100までを表示する
+ *  @return {void}
+ */
+function showHundred1(){
+    for (let i = 0; i <= 100; i++) {
+        console.log(i)
+    }
+}
+
+/**
+ *  0から100までを表示する。5の倍数ごとに改行する。
+ *  @return {void}
+ */
+function showHundred2(){
+    for (let i = 0; i <= 100; i++) {
+        if (i % 5 === 0) {
+            //process.stdout.write("\n")
+            console.log("\n")
+        }
+        console.log(i)
+        //process.stdout.write((i.toString() + " "))
+        //terminalでうまく改行されない
+    }
+}
+
+/**
+ *  3つの文字列を連結した文字列を返す
+ *  @param {string} str1 文字列1
+ *  @param {string} str2 文字列2
+ *  @param {string} str3 文字列3
+ *  @return {string} 連結した文字列
+ */
+function mergeString(str1: string, str2: string, str3: string): string
+{
+    return str1 + str2 + str3
 }
