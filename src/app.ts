fce622f7513c2e64ee721307f70fc84aef232295
Soichiro Inatani <inai17ibar@gmail.com>
2023-12-24 11:55:34 +0900

    solved Problem2

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-console.log(getHello())
+console.log(getHelloWithDate())
 
 /**
  *  「こんにちは」を取得する
@@ -7,4 +7,24 @@ console.log(getHello())
 function getHello(): string
 {
     return "こんにちは"
+}
+
+/**
+ *  時間によって挨拶を返す関数
+ *  @return {string} 文字列
+ */
+function getHelloWithDate(): string
+{
+    const date = new Date()
+    const hour = date.getHours()
+
+    if (hour >= 5 && hour < 12) {
+        return "おはよう"
+    }else if (hour >= 12 && hour < 18) {
+        return "こんにちは"
+    }else if (hour >= 18 && hour < 0) {
+        return "こんばんは"
+    }else {
+        return "おやすみ"
+    }
 }
